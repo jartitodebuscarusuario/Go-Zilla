1933d783721a151bd179fc45742e6f2b3f6f50b1
Ariadnaya <Ariadnaya@Ariadnaya-PC>
2017-12-11 18:21:33 +0100

    11/12/2017

--- GimSelfProv/src/TestGetCpuMem/TestGetCpuMem.go
+++ GimSelfProv/src/TestGetCpuMem/TestGetCpuMem.go
@@ -2,15 +2,31 @@ package main
 
 import (
     "fmt"
-    "github.com/shirou/gopsutil/mem"
+    "time"
+    //"github.com/shirou/gopsutil/mem"
+    //"github.com/shirou/gopsutil/load"
+    "github.com/shirou/gopsutil/cpu"
 )
 
 func main() {
-    v, _ := mem.VirtualMemory()
+    //v, _ := mem.VirtualMemory()
 
     // almost every return value is a struct
-    fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+    //fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
 
     // convert to JSON. String() is also implemented
-    fmt.Println(v)
+    //fmt.Println(v)
+    
+    //v, _ := load.Avg()
+    
+    // almost every return value is a struct
+    //fmt.Printf("Load1: %v, Load5:%v, Load15:%v\n", v.Load1, v.Load5, v.Load15)
+
+    // convert to JSON. String() is also implemented
+    //fmt.Println(v)
+    
+    v, _ := cpu.Percent(15 * time.Second, false)
+    
+    fmt.Printf("Percent(15, false): %v\n", v)
+    
 }
